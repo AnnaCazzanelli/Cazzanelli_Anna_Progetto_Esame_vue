4c9c90a603e7c1ae05d4f330b39d3c7fa98a7a86
Anna <anna.cazzanelli1@gmail.com>
2025-09-17 18:52:42 +0200

    Fixed CTA and Footer in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <script setup>
+import { useRoute } from 'vue-router'
 import Navbar from './components/Navbar.vue'
 import CtaSection from './components/CtaSection.vue'
+import Footer from './components/Footer.vue'
+
+const route = useRoute()
 </script>
 
 <template>
-  <Navbar />
-  <RouterView />
-  <CtaSection />
+  <div class="app-wrapper">
+    <Navbar />
+    <RouterView />
+
+    <!-- Mostra CTA e Footer solo se non sei nella pagina Contatti -->
+    <template v-if="route.name !== 'contacts'">
+      <CtaSection />
+      <Footer />
+    </template>
+  </div>
 </template>
